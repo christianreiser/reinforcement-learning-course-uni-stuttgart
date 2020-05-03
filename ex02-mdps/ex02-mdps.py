6406b887cb3fda391a6f44a085e0baa3563cc9d9
christianreiser <reisercg@gmail.com>
2020-05-03 16:22:38 +0200

    ex2b implemented, but might be wrong

--- ex02-mdps/ex02-mdps.py
+++ ex02-mdps/ex02-mdps.py
@@ -10,24 +10,26 @@ custom_map3x3 = [
 ]
 env = gym.make("FrozenLake-v0", desc=custom_map3x3)
 # TODO: Uncomment the following line to try the default map (4x4):
-#env = gym.make("FrozenLake-v0")
+#  env = gym.make("FrozenLake-v0")
 
 # Uncomment the following lines for even larger maps:
-#random_map = generate_random_map(size=5, p=0.8)
-#env = gym.make("FrozenLake-v0", desc=random_map)
+# random_map = generate_random_map(size=5, p=0.8)
+# env = gym.make("FrozenLake-v0", desc=random_map)
 
 # Init some useful variables:
 n_states = env.observation_space.n
 n_actions = env.action_space.n
 
-r = np.zeros(n_states) # the r vector is zero everywhere except for the goal state (last state)
+r = np.zeros(n_states)  # the r vector is zero everywhere except for the goal state (last state)
 r[-1] = 1.
 
 gamma = 0.8
 
 
-""" This is a helper function that returns the transition probability matrix P for a policy """
 def trans_matrix_for_policy(policy):
+    """
+    This is a helper function that returns the transition probability matrix P for a policy
+    """
     transitions = np.zeros((n_states, n_states))
     for s in range(n_states):
         probs = env.P[s][policy[s]]
@@ -36,8 +38,10 @@ def trans_matrix_for_policy(policy):
     return transitions
 
 
-""" This is a helper function that returns terminal states """
 def terminals():
+    """
+    This is a helper function that returns terminal states
+    """
     terms = []
     for s in range(n_states):
         # terminal is when we end with probability 1 in terminal:
@@ -48,26 +52,36 @@ def terminals():
 
 def value_policy(policy):
     P = trans_matrix_for_policy(policy)
-    # TODO: calculate and return v
+    # TODO: sth is wrong: calculate and return v
     # (P, r and gamma already given)
-    return None
+    # v= (−γP+Id)^(-1) * r
+    v = np.matmul(
+            np.linalg.inv(
+                np.multiply(-gamma, P) + np.identity(n_states)
+            )
+        , r
+    )
+    return v
 
 
 def bruteforce_policies():
     terms = terminals()
     optimalpolicies = []
 
-    policy = np.zeros(n_states, dtype=np.int)  # in the discrete case a policy is just an array with action = policy[state]
-    optimalvalue = np.zeros(n_states)
-    
-    # TODO: implement code that tries all possible policies, calculate the values using def value_policy. Find the optimal values and the optimal policies to answer the exercise questions.
+    # in the discrete case a policy is just an array with action = policy[state]
+    policy = np.zeros(n_states, dtype=np.int)
 
-    print ("Optimal value function:")
+    optimalvalue = np.zeros(n_states)
+
+    # TODO: implement code that tries all possible policies, calculate the values using def value_policy.
+    # TODO: Find the optimal values and the optimal policies to answer the exercise questions.
+
+    print("Optimal value function:")
     print(optimalvalue)
-    print ("number optimal policies:")
-    print (len(optimalpolicies))
-    print ("optimal policies:")
-    print (np.array(optimalpolicies))
+    print("number optimal policies:")
+    print(len(optimalpolicies))
+    print("optimal policies:")
+    print(np.array(optimalpolicies))
     return optimalpolicies
 
 
@@ -84,12 +98,11 @@ def main():
 
     # Value functions:
     print("Value function for policy_left (always going left):")
-    print (value_policy(policy_left))
+    print(value_policy(policy_left))
     print("Value function for policy_right (always going right):")
     print (value_policy(policy_right))
 
     optimalpolicies = bruteforce_policies()
-
 
     # This code can be used to "rollout" a policy in the environment:
     """
