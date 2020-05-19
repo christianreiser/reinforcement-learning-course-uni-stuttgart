38b3330285a6a4de0121d131b89d2f0c40f94340
christianreiser <reisercg@gmail.com>
2020-05-19 23:54:28 +0200

    ex fro upload

--- ex04-mc/ex04-mc.py
+++ ex04-mc/ex04-mc.py
@@ -1,23 +1,119 @@
+# code slightly modified from https://www.kaggle.com/hamishdickson/blackjack-with-reinforcement-learning
+
 import gym
+
+import random
+from collections import defaultdict
 import numpy as np
 
+# this is all plotting stuff :/
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter, MaxNLocator
+#%matplotlib inline
 
-def main():
-    # This example shows how to perform a single run with the policy that hits for player_sum >= 20
-    env = gym.make('Blackjack-v0')
-    obs = env.reset()  # obs is a tuple: (player_sum, dealer_card, useable_ace)
-    done = False
-    while not done:
-        print("observation:", obs)
-        if obs[0] >= 20:
-            print("stick")
-            obs, reward, done, _ = env.step(0)
-        else:
-            print("hit")
-            obs, reward, done, _ = env.step(1)
-        print("reward:", reward)
-        print("")
+matplotlib.style.use('ggplot')
 
 
-if __name__ == "__main__":
-    main()
+env = gym.make('Blackjack-v0')
+episodes = 50000
+gamma = 1.
+
+
+def get_epsilon(N_state_count, N_zero=100):
+    return N_zero / (N_zero + N_state_count)
+
+def get_action(Q, state, state_count, action_size):
+    random_action = random.randint(0, action_size - 1)
+    best_action = np.argmax(Q[state])
+    epsilon = get_epsilon(state_count)
+    return np.random.choice([best_action, random_action], p=[1. - epsilon, epsilon])
+
+
+def evaluate_policy(Q, episodes=50000):
+    wins = 0
+    for _ in range(episodes):
+        state = env.reset()
+        done = False
+        while not done:
+            action = np.argmax(Q[state])
+            state, reward, done, _ = env.step(action=action)
+        if reward > 0:
+            wins += 1
+    return wins / episodes
+
+
+def monte_carlo(gamma=1., episodes=50000, evaluate=False):
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    state_count = defaultdict(float)
+    state_action_count = defaultdict(float)
+    evaluations = []
+
+    for i in range(episodes):
+        if evaluate and i % 1000 == 0:
+            evaluations.append(evaluate_policy(Q))
+        episode = []
+        state = env.reset()
+        done = False
+        while not done:
+            state_count[state] += 1
+            action = get_action(Q, state, state_count[state], env.action_space.n)
+            new_state, reward, done, _ = env.step(action=action)
+            episode.append((state, action, reward))
+            state = new_state
+        G = 0
+
+        for s, a, r in reversed(episode):
+            new_s_a_count = state_action_count[(s, a)] + 1
+            G = r + gamma * G
+            state_action_count[(s, a)] = new_s_a_count
+            Q[s][a] = Q[s][a] + (G - Q[s][a]) / new_s_a_count
+
+    return Q, evaluations
+
+
+def plot_value_function(Q, title="Value Function"):
+    V = defaultdict(float)
+
+    for state, action_rewards in Q.items():
+        r1, r2 = action_rewards
+        action_value = np.max([r1, r2])
+        V[state] = action_value
+
+    min_x = min(k[0] for k in V.keys())
+    max_x = max(k[0] for k in V.keys())
+    min_y = min(k[1] for k in V.keys())
+    max_y = max(k[1] for k in V.keys())
+
+    x_range = np.arange(min_x, max_x + 1)
+    y_range = np.arange(min_y, max_y + 1)
+    X, Y = np.meshgrid(x_range, y_range)
+
+    # Find value for all (x, y) coordinates
+    Z_noace = np.apply_along_axis(lambda _: V[(_[0], _[1], False)], 2, np.dstack([X, Y]))
+    Z_ace = np.apply_along_axis(lambda _: V[(_[0], _[1], True)], 2, np.dstack([X, Y]))
+
+    def plot_surface(X, Y, Z, title):
+        fig = plt.figure(figsize=(20, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                               cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+        ax.set_xlabel('Player sum')
+        ax.set_ylabel('Dealer showing')
+        ax.set_zlabel('Value')
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.set_title(title)
+        ax.view_init(ax.elev, 120)
+        fig.colorbar(surf)
+        plt.show()
+
+    plot_surface(X, Y, Z_noace, "value function")
+    plot_surface(X, Y, Z_ace, "value function - usable ace")
+
+
+Q_mc, evaluations = monte_carlo(episodes=50000, evaluate=True)
+plot_value_function(Q_mc)
+print('policy:', Q_mc)
